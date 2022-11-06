b9b7e53b0babf07bad5680c55af29eec698f43b2
LSchnuriger <45211906+LoicSCHN@users.noreply.github.com>
2022-11-06 12:06:17 +0100

    Version sans commentaire ni affichage

---
+++ generator.py
@@ -0,0 +1,30 @@
+import random
+import csv
+
+n = int(input("Entrez le nombre d'élève et établissement : "))
+#n = 100
+
+
+listeEleve = []
+listeEtablissement = []
+
+
+for i in range(0,n):
+    listeEleve.append(i)
+    listeEtablissement.append(i)
+
+with open('fichierEleves.csv', 'w', newline='') as f:
+    writter = csv.writer(f)
+    for i in range(0,len(listeEleve)):  
+        random.shuffle(listeEleve)  
+        writter.writerow(listeEleve)
+
+f.close()
+
+with open('fichierEtablissements.csv', 'w', newline='') as f:
+    writter = csv.writer(f)
+    for i in range(0,len(listeEtablissement)): 
+        random.shuffle(listeEtablissement)  
+        writter.writerow(listeEtablissement)
+
+f.close()
---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+import subprocess
+subprocess.check_call([sys.executable, "-m", "pip", "install", "pandas"])
+import pandas as pd
+
+subprocess.call("generator.py", shell=True)
+prefEtudiant = pd.read_csv("fichierEleves.csv", header=None, sep = ",").values.tolist() 
+
+prefEtablissement = pd.read_csv("fichierEtablissements.csv", header=None, sep = ",").values.tolist()
+
+def attribution(n, prefEtudiant, prefEtablissement):
+    etudiantLibre = list(range(n))
+    affectationEtudiant = [None] * n     
+    affectationEtablissement = [None] * n  
+    suivant = [0] * n                     
+    etape = 0
+    while etudiantLibre:
+        etape = etape +1
+        etudiant = etudiantLibre[0] 
+        prefEtudiantLibre = prefEtudiant[etudiant]  
+        etablissement = prefEtudiantLibre[suivant[etudiant]] 
+        prefEtablissementEleve = prefEtablissement[etablissement]
+        etudiantActuel = affectationEtablissement[etablissement]
+       
+        
+        if etudiantActuel == None:
+          affectationEtablissement[etablissement] = etudiant
+          affectationEtudiant[etudiant] = etablissement
+          suivant[etudiant] = suivant[etudiant] + 1
+          etudiantLibre.pop(0)
+        else:
+          indexEtudiantActuel = prefEtablissementEleve.index(etudiantActuel)
+          indexEtudiant2 = prefEtablissementEleve.index(etudiant)
+          
+          if indexEtudiantActuel > indexEtudiant2:
+             affectationEtablissement[etablissement] = etudiant
+             affectationEtudiant[etudiant] = etablissement
+             suivant[etudiant] = suivant[etudiant] + 1
+             etudiantLibre.pop(0)
+             etudiantLibre.insert(0,etudiantActuel)
+          else:
+            suivant[etudiant] = suivant[etudiant] + 1
+             
+  
+    return affectationEtudiant
+
+
+res = attribution(len(prefEtudiant), prefEtudiant, prefEtablissement)
+print("  ")
+print("----------------------------------RESULTATS-----------------------------------------")
+print("  ")
+print(res)
